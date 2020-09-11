19517475df6eb180fa13bf27ddde7638a7733940
wuyikai <wyk1016@126.com>
2020-09-11 11:32:09 +0800

    notification content extension

---
+++ NotificationContent/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  NotificationContent
+//
+//  Created by DouKing on 2020/9/11.
+//  Copyright © 2020 secoo. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
